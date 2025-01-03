833ce596852b936692fa306f10226808c815a152
joanalnu <joanalnu@outlook.com>
2025-01-03 18:30:38 +0100

    move messages to own file, add temporary message guide file

---
+++ v4/messages.js
@@ -0,0 +1,200 @@
+const messages = {
+    titles: {
+        "en": "Mental Calculation Trainer",
+        "es": "Entrenador de Cálculo Mental",
+        "ca": "Entrenador de Càlcul Mental",
+        "de": "Mentalrechner Trainer",
+        "fr": "Entraîneur de Calcul Mental",
+        "it": "Allenatore di Calcolo Mentale",
+        "pt": "Treinador de Cálculo Mental",
+        "zh": "心算训练员",
+        "ja": "メンタル計算トレーナー",
+        "ko": "멘탈 계산 트레이너"
+    },
+    footers: {
+        "en": "© 2024 All Rights Reserved | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>Instructions</a>",
+        "es": "© 2024 Todos los derechos reservados | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>Instrucciones</a>",
+        "ca": "© 2024 Tots els drets reservats | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>Instruccions</a>",
+        "de": "© 2024 Alle Rechte vorbehalten | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>Anleitung</a>",
+        "fr": "© 2024 Tous droits réservés | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>Instructions</a>",
+        "it": "© 2024 Tutti i diritti riservati | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>Istruzioni</a>",
+        "pt": "© 2024 Todos os direitos reservados | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>Instruções</a>",
+        "zh": "© 2024 版权所有 | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>说明</a>",
+        "ja": "© 2024 すべての権利予約 | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>説明</a>",
+        "ko": "© 2024 모든 권한 보유 | <a href='https://github.com/joanalnu/TdM/blob/main/README.md'>설명</a>"
+    },
+    continues: {
+        "en": "Continue",
+        "es": "Continuar",
+        "ca": "Continuar",
+        "de": "Fortsetzen",
+        "fr": "Continuer",
+        "it": "Continuare",
+        "pt": "Continuar",
+        "zh": "继续",
+        "ja": "続ける",
+        "ko": "계속하기"
+    },
+    welcomeMessages: {
+        "en": "Hello, I'm your mental calculation trainer.",
+        "es": "Hola, soy tu entrenador de cálculo mental.",
+        "ca": "Hola, soc el teu entrenador de càlcul mental.",
+        "de": "Hallo, ich bin Ihr Trainer für Kopfrechnen.",
+        "fr": "Bonjour, je suis votre entraîneur en calcul mental.",
+        "it": "Salve, sono il vostro allenatore di calcolo mentale.",
+        "pt": "Olá, sou o vosso treinador de cálculo mental.",
+        "zh": "你好，我是你的心算培训师。",
+        "ja": "こんにちは、私はあなたのメンタル計算トレーナーです。",
+        "ko": "안녕하세요, 저는 여러분의 멘탈 계산 트레이너입니다."
+    },
+    modeSelectionTitles: {
+        "en": "Select a Mode",
+        "es": "Seleccione un Modo",
+        "ca": "Seleccioni el Mode",
+        "de": "Wählen Sie Modus",
+        "fr": "Sélectionner le mode",
+        "it": "Selezionare la modalità",
+        "pt": "Selecionar o modo",
+        "zh": "选择模式",
+        "ja": "モード選択",
+        "ko": "모드 선택"
+    },
+    modeButtons: {
+        "mode1": {
+            "en": "Training Mode",
+            "es": "Modo de Entrenamiento",
+            "ca": "Mode d'Entrenament",
+            "de": "Trainingsmodus",
+            "fr": "Mode d'Entraînement",
+            "it": "Modalità di Allenamento",
+            "pt": "Modo de Treinamento",
+            "zh": "培训模式",
+            "ja": "トレーニングモード",
+            "ko": "교육 모드"
+        },
+        "mode2": {
+            "en": "Gaming Mode",
+            "es": "Modo de Juego",
+            "ca": "Mode de Joc",
+            "de": "Spielmodus",
+            "fr": "Mode de Jeu",
+            "it": "Modalità di Gioco",
+            "pt": "Modo de Jogo",
+            "zh": "游戏模式",
+            "ja": "ゲーミング・モード",
+            "ko": "게임 모드"
+        }
+    },
+    calcButtons: {
+        "calc1": {
+            "en": "Addition",
+            "es": "Suma",
+            "ca": "Suma",
+            "de": "Addition",
+            "fr": "Addition",
+            "it": "Addizione",
+            "pt": "Adição",
+            "zh": "加法",
+            "ja": "加算",
+            "ko": "더하기"
+        },
+        "calc2": {
+            "en": "Subtraction",
+            "es": "Resta",
+            "ca": "Resta",
+            "de": "Subtraktion",
+            "fr": "Soustraction",
+            "it": "Sottrazione",
+            "pt": "Subtração",
+            "zh": "减法",
+            "ja": "減算",
+            "ko": "빼기"
+        },
+        "calc3": {
+            "en": "Multiplication",
+            "es": "Multiplicación",
+            "ca": "Multiplicació",
+            "de": "Multiplikation",
+            "fr": "Multiplication",
+            "it": "Moltiplicazione",
+            "pt": "Multiplicação",
+            "zh": "乘法",
+            "ja": "乗算",
+            "ko": "곱하기"
+        },
+        "calc4": {
+            "en": "Division",
+            "es": "División",
+            "ca": "Divisió",
+            "de": "Division",
+            "fr": "Division",
+            "it": "Divisione",
+            "pt": "Divisão",
+            "zh": "除法",
+            "ja": "除算",
+            "ko": "나누기"
+        }
+    },
+    submitButton: {
+        "en": "Submit",
+        "es": "Enviar",
+        "ca": "Enviar",
+        "de": "Senden",
+        "fr": "Soumettre",
+        "it": "Invia",
+        "pt": "Enviar",
+        "zh": "提交",
+        "ja": "送信",
+        "ko": "제출"
+    },
+    quitButton: {
+        "en": "Quit",
+        "es": "Salir",
+        "ca": "Sortir",
+        "de": "Beenden",
+        "fr": "Quitter",
+        "it": "Esci",
+        "pt": "Sair",
+        "zh": "退出",
+        "ja": "終了",
+        "ko": "종료"
+    },
+    yourAnswerText: {
+        "en": "Your answer",
+        "es": "Tu respuesta",
+        "ca": "La teva resposta",
+        "de": "Deine Antwort",
+        "fr": "Votre réponse",
+        "it": "La tua risposta",
+        "pt": "Sua resposta",
+        "zh": "你的答案",
+        "ja": "あなたの答え",
+        "ko": "당신의 답"
+    },
+    correctMessages: {
+        "en": "Correct! Points: ",
+        "es": "¡Correcto! Puntos: ",
+        "ca": "Correcte! Punts: ",
+        "de": "Korrekt! Punkte: ",
+        "fr": "Correct! Points: ",
+        "it": "Corretto! Punti: ",
+        "pt": "Correto! Pontos: ",
+        "zh": "正确！分数：",
+        "ja": "正解！ポイント：",
+        "ko": "정답! 포인트: "
+    },
+    incorrectMessages: {
+        "en": "Oh no! The correct answer was: ",
+        "es": "¡Oh no! La respuesta correcta era: ",
+        "ca": "Oh no! La resposta correcta era: ",
+        "de": "Oh nein! Die richtige Antwort war: ",
+        "fr": "Oh non! La bonne réponse était : ",
+        "it": "Oh no! La risposta giusta era: ",
+        "pt": "Oh não! A resposta correta era: ",
+        "zh": "哦不！正确答案是：",
+        "ja": "おっと！正しい答えは：",
+        "ko": "오 안돼! 정답은: "
+    }
+};
+
+export default messages;
